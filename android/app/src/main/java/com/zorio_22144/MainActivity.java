4f7ddd45461b928201e7bd72a9168bfc4f1e7784
Crowdbotics <team@crowdbotics.com>
2020-10-31 00:21:33 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/zorio_22144/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zorio_22144;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "zorio_22144";
+    }
+}
